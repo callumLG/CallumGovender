828c5ae6dfb3819dd26605c978fd39d62c49e69d
Callum Govender <callumGovender06@gmail.com>
2026-02-20 22:15:37 +0200

    fixed card styling

--- portfolio/app/components/Skills.jsx
+++ portfolio/app/components/Skills.jsx
@@ -44,13 +44,15 @@ const skills = [
       "Version control, branching, commits, and managing personal projects",
   },
 ];
-
 function Skills() {
   return (
-    <div>
-      <div className="mx-auto bg-[#111111] text-white">
-        <h1 className="text-5xl font-bold text-center ">Skills</h1>
-        <div>
+    <div className="mx-auto bg-[#111111] text-white py-24">
+      <div
+        className="max-w-6xl mx-auto
+"
+      >
+        <h1 className="text-5xl font-bold text-center mb-12 ">Skills</h1>
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-5 md:gap-6">
           {skills.map((skill) => {
             const Icon = skill.icon;
             return (
