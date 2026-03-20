58ae12c066a1fb25250aa310f0349b5c21514975
Callum Govender <callumGovender06@gmail.com>
2026-03-20 09:39:25 +0200

    made dynamic and responsive changes

--- portfolio/app/components/About.jsx
+++ portfolio/app/components/About.jsx
@@ -1,49 +1,67 @@
 import React from "react";
 import { Github, Linkedin } from "lucide-react";
+import Image from "next/image";
+
 function About() {
+  const techStack = [
+    { name: "Html5", color: "#61DAFB" },
+    { name: "CSS3", color: "#61DAFB" },
+    { name: "JavaScript", color: "#F7DF1E" },
+    { name: "React", color: "#61DAFB" },
+    { name: "Next.js", color: "#000000" },
+    { name: "Tailwind CSS", color: "#38B2AC" },
+  ];
   return (
     <div
       id="about"
-      className="text-white tracking-tight mx-auto my-40 flex flex-col items-center gap-4 max-w-3xl text-center"
+      className="w-full text-white tracking-tight mx-auto my-40 flex flex-col items-center gap-4 max-w-3xl text-center"
     >
       <h1 className="text-5xl font-bold mb-12 text-center">About Me</h1>
-      <div className="flex flex-col md:flex-row items-center gap-8">
-        <p className="text-base  leading-relaxed opacity-90 w-screen px-5 lg:w-250 text-left">
-          I'm a junior web developer focused on building clean, reliable web
-          interfaces using modern tools. I care about structure, readability,
-          and writing code that's easy to maintain. I'm actively improving my
-          skills through hands-on projects and learning by building, breaking,
-          and fixing real things.
+      <div className="flex flex-col lg:flex-row items-center gap-8">
+        <p className="w-screen lg:w-157.5 text-base leading-relaxed opacity-90  px-5  text-center md:text-left ">
+          I&apos;m a junior web developer focused on building clean, reliable
+          web interfaces using modern tools. I care about structure,
+          readability, and writing code that&apos;s easy to maintain. I&apos;m
+          actively improving my skills through hands-on projects and learning by
+          building, breaking, and fixing real things.
         </p>
-        <img
-          src="/avitar.avif"
-          alt="avitar"
-          className="w-50  lg:w-100  rounded-full ml-4 border-2 border-gray-800 "
-        />
-        <div className="flex flex-row items-center justify-center gap-3 sm:gap-4 lg:flex-col">
-          {/* <a
-            href="https://www.linkedin.com/in/callum-govender-027a9b29a/"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="LinkedIn"
-            className="flex items-center gap-2 rounded-full border border-white/20 px-3 py-2 sm:px-4 sm:py-2 hover:text-[#A8C5E8] hover:border-[#A8C5E8]/50 transition-all duration-300"
-          >
-            <Linkedin className="w-5 h-5" />
-            <span className="hidden sm:inline text-sm font-medium">
-              LinkedIn
-            </span>
-          </a> */}
 
-          <a
-            href="https://github.com/callumLG"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="GitHub"
-            className="flex items-center gap-2 rounded-full border border-white/20 px-3 py-2 sm:px-4 sm:py-2 hover:text-[#A8C5E8] hover:border-[#A8C5E8]/50 transition-all duration-300"
-          >
-            <Github className="w-5 h-5" />
-            <span className="hidden sm:inline text-sm font-medium">GitHub</span>
-          </a>
+        <div className="flex gap-5">
+          <Image
+            src="/avitar.avif"
+            alt="avitar"
+            width={200}
+            height={200}
+            className="w-50  lg:w-100  rounded-full ml-4 border-2 border-gray-800 "
+          />
+          {/* links */}
+          <div className="flex flex-col items-center justify-center gap-3 sm:gap-4 lg:flex-col">
+            <a
+              href="https://www.linkedin.com/in/callum-govender-027a9b29a/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="LinkedIn"
+              className="flex items-center gap-2 rounded-full border border-white/20 px-3 py-2 sm:px-4 sm:py-2 hover:text-[#A8C5E8] hover:border-[#A8C5E8]/50 transition-all duration-300"
+            >
+              <Linkedin className="w-5 h-5" />
+              <span className="hidden sm:inline text-sm font-medium">
+                LinkedIn
+              </span>
+            </a>
+
+            <a
+              href="https://github.com/callumLG"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="GitHub"
+              className="flex items-center gap-2 rounded-full border border-white/20 px-3 py-2 sm:px-4 sm:py-2 hover:text-[#A8C5E8] hover:border-[#A8C5E8]/50 transition-all duration-300"
+            >
+              <Github className="w-5 h-5" />
+              <span className="hidden sm:inline text-sm font-medium">
+                GitHub
+              </span>
+            </a>
+          </div>
         </div>
       </div>
       <div className="px-4 py-8">
@@ -52,18 +70,14 @@ function About() {
         </h2>
 
         <div className="flex flex-wrap justify-center gap-3 sm:gap-4">
-          <p className="text-sm sm:text-base rounded-full border border-gray-400 px-4 py-2 hover:border-[#A8C5E8] transition-all duration-300">
-            JavaScript
-          </p>
-          <p className="text-sm sm:text-base rounded-full border border-gray-400 px-4 py-2 hover:border-[#A8C5E8] transition-all duration-300">
-            React
-          </p>
-          <p className="text-sm sm:text-base rounded-full border border-gray-400 px-4 py-2 hover:border-[#A8C5E8] transition-all duration-300">
-            Next.js
-          </p>
-          <p className="text-sm sm:text-base rounded-full border border-gray-400 px-4 py-2 hover:border-[#A8C5E8] transition-all duration-300">
-            Tailwind CSS
-          </p>
+          {techStack.map((tech) => (
+            <span
+              key={tech.name}
+              className="text-sm sm:text-base rounded-full border border-gray-400 px-4 py-2 hover:border-[#A8C5E8] transition-all duration-300"
+            >
+              {tech.name}
+            </span>
+          ))}
         </div>
       </div>
     </div>
--- portfolio/app/components/Greet.jsx
+++ portfolio/app/components/Greet.jsx
@@ -6,15 +6,17 @@ import LocalTime from "./LocalTime";
 function Greet() {
   return (
     <div className="text-center  my-62.5 lg:my-87.5 flex flex-col gap-4 tracking-tight">
-      <h1 className="text-7xl font-bold text-white ">Callum Govender</h1>
+      <h1 className="text-5xl sm:text-7xl font-bold text-white ">
+        Callum Govender
+      </h1>
       <p className="text-2xl text-gray-300 mt-4">
         Junior Web Developer / Frontend Developer
       </p>
-      <div className="flex gap-1 mx-auto w-max mt-4">
+      <div className="flex gap-1 mx-auto w-max mt-4 text-xs ">
         <a
           href="https://www.google.com/search?gs_ssp=eJzj4tTP1TcwLMkqKzNg9GJLKS1KSswDADk2Bgc&q=durban&rlz=1C1CHZN_enZA1118ZA1118&oq=durban&gs_lcrp=EgZjaHJvbWUqCggBEC4YsQMYgAQyBwgAEAAYjwIyCggBEC4YsQMYgAQyCggCEAAYsQMYgAQyCggDEAAYsQMYgAQyCggEEC4YsQMYgAQyBggFEEUYPDIGCAYQRRg8MgYIBxBFGD3SAQgyOTU3ajBqMagCALACAA&sourceid=chrome&ie=UTF-8"
           target="_blank"
-          className="cursor-pointer text-white text-lg justify-center items-center border-2 border-gray-800 rounded-full px-4 py-2   mt-4  hover:border-[#A8C5E8] transition-all duration-300"
+          className="cursor-pointer text-white justify-center items-center border-2 border-gray-800 rounded-full px-4 py-2   mt-4  hover:border-[#A8C5E8] transition-all duration-300"
         >
           <MapPin className="inline-block mr-1 text-white" size={18} />
           Durban, South Africa
@@ -26,19 +28,19 @@ function Greet() {
             className="ml-1 inline-block"
           />
         </a>
-        <div className=" text-white text-lg  justify-center items-center border-2 border-gray-800 rounded-full px-4 py-2   mt-4  hover:border-[#A8C5E8] transition-all duration-300">
+        <div className=" text-white justify-center items-center border-2 border-gray-800 rounded-full px-4 py-2   mt-4  hover:border-[#A8C5E8] transition-all duration-300">
           <LocalTime />
         </div>
       </div>
       <div className=" flex gap-2.5 mx-auto my-8">
-        <button className="text-[#A8C5E8] w-50 border-2 rounded-lg py-3 hover:bg-[#A8C5E8] hover:text-black transition duration-300 cursor-pointer  flex justify-center gap-1">
+        <button className="text-[#A8C5E8] w-fit md:w-50 flex px-2 border-2 rounded-lg py-3 hover:bg-[#A8C5E8] hover:text-black transition duration-300 cursor-pointer justify-center gap-1">
           <a href="#projects"> View Projects</a>
           <ArrowRight />
         </button>
         <a
           href="/callumGovenderCV.docx"
           download="callumGovenderCV.docx"
-          className="text-[#F5A3A3] w-50 border-2 rounded-lg py-3  hover:bg-[#F5A3A3] hover:text-black transition duration-300 inline-block"
+          className="text-[#F5A3A3] w-fit md:w-50 px-5 border-2 rounded-lg py-3  hover:bg-[#F5A3A3] hover:text-black transition duration-300 inline-block"
         >
           CV
         </a>
